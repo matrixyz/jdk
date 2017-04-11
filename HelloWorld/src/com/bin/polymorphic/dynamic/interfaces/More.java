351a0057d3a026273a08eb0271e7c92dc6497df5
hb <hb@DESKTOP-5PFN5PJ>
2017-04-11 09:53:08 +0800

    a

---
+++ HelloWorld/src/com/bin/polymorphic/dynamic/interfaces/More.java
@@ -0,0 +1,32 @@
+package com.bin.polymorphic.dynamic.interfaces;
+
+ interface Mores {
+	public void test();
+
+}
+
+class TestImplA implements Mores{
+
+	public void test() {
+		// TODO Auto-generated method stub
+		System.out.println("TestImplA.test()");
+	}
+	
+}
+class TestImplB implements Mores{
+
+	public void test() {
+		// TODO Auto-generated method stub
+		System.out.println("TestImplA.test()");
+	}
+	
+}
+public class More{
+	public static void main(String[] args) {
+		Mores more=null;
+		//*********
+		more.test();
+		
+		
+	}
+}
